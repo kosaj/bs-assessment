2c45647f06d47dffa2f5f090f073feb6351f4814
kosaj <kosaj@users.noreply.github.com>
2022-11-26 23:41:05 +0100

    .

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { DashboardComponent } from "@app/pages/dashboard/dashboard.component";
 
 export const APP_ROUTES: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/dashboard" },
