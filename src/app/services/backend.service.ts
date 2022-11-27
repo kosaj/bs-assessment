b10e6eb194744c364e04de9ea1c91bb6cd06704b
kosaj <kosaj@users.noreply.github.com>
2022-11-28 00:55:00 +0100

    .

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from "@angular/core";
 import { Environment } from "@app/models/environment.interface";
 import { EnvironmentToken } from "@app/tokens/environment.token";
 import { Observable } from "rxjs";
+import { Manager } from "socket.io-client";
 
 @Injectable({
   providedIn: "root",
@@ -11,17 +12,55 @@ export class BackendService {
   private readonly _apiUrl =
     this._environment.configuration.backendConfig.apiUrl;
 
+  private readonly manager = new Manager(this._apiUrl);
+
   constructor(
     @Inject(EnvironmentToken)
     private readonly _environment: Environment,
     private readonly _httpClient: HttpClient
   ) {}
 
-  getBets(): Observable<any> {
-    return this._httpClient.get(`${this._apiUrl}`);
+  /**
+   * returns an array of `bet`.
+   * @returns
+   */
+  getBets(): Observable<Array<any>> {
+    return this._httpClient.get<Array<any>>(`${this._apiUrl}/bets`);
   }
 
+  /**
+   * returns the `bet` object for the given id
+   * @param id
+   * @returns
+   */
   getBet(id: number): Observable<any> {
-    return this._httpClient.get(`${this._apiUrl}/bets/${id}`);
+    return this._httpClient.get<any>(`${this._apiUrl}/bets/${id}`);
+  }
+
+  /**
+   * generates random bets for the given size
+   * @param size
+   * @returns
+   */
+  generateBets(size: number): Observable<any> {
+    return this._httpClient.get<any>(
+      `${this._apiUrl}/bets-generate?size=${size}`
+    );
+  }
+
+  /**
+   * starts the websocket pulling
+   * @param rate
+   * @returns
+   */
+  startPulling(rate: number): Observable<any> {
+    return this._httpClient.get(`${this._apiUrl}/pulling/start?rate=${rate}`);
+  }
+
+  /**
+   * Stop websocket pulling
+   */
+  stopPulling(): Observable<any> {
+    return this._httpClient.get(`${this._apiUrl}/pulling/stop`);
   }
 }
