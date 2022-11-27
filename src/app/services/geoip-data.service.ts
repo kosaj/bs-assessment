aeee36dfdda302f6ceeccbb7443378ebc3955a1e
kosaj <kosaj@users.noreply.github.com>
2022-11-27 23:27:56 +0100

    .

---
+++ src/app/services/geoip-data.service.ts
@@ -0,0 +1,131 @@
+import { HttpClient } from "@angular/common/http";
+import {
+  APP_INITIALIZER,
+  Inject,
+  Injectable,
+  Injector,
+  OnDestroy,
+  Provider,
+} from "@angular/core";
+import { Environment, EnvironmentToken } from "@app/tokens/environment.token";
+import {
+  BehaviorSubject,
+  defer,
+  filter,
+  map,
+  Observable,
+  of,
+  Subject,
+  takeUntil,
+  tap,
+} from "rxjs";
+
+export interface GeoipData {
+  currency: Currency;
+  location: Location;
+}
+
+interface Currency {
+  code: string;
+  symbol: string;
+  symbolNative: string;
+}
+
+interface Location {
+  continent: Continent;
+  country: Country;
+}
+
+interface Country {
+  code: string;
+  name: string;
+  inEU: boolean;
+}
+interface Continent {
+  code: string;
+  name: string;
+}
+
+export const GeoipAppInitializerProvider: Provider = {
+  provide: APP_INITIALIZER,
+  deps: [Injector],
+  multi: true,
+  useFactory: (injector: Injector) =>
+    injector
+      .get(GeoipDataService)
+      .initialized$.pipe(filter((result) => result)),
+};
+
+const DEFAULT_GEOIP_DATA: GeoipData = {
+  currency: { code: "PLN", symbol: "PLN", symbolNative: "zł" },
+  location: {
+    continent: { code: "EU", name: "Europe" },
+    country: { code: "PL", inEU: true, name: "Poland" },
+  },
+};
+
+@Injectable({
+  providedIn: "root",
+})
+export class GeoipDataService implements OnDestroy {
+  private readonly _destroyed = new Subject<void>();
+  private readonly _initializedSource = new BehaviorSubject<boolean>(false);
+  private readonly _geoipData$: Observable<GeoipData> = defer(() => {
+    const { apiUrl, apiKey } = this._environment.configuration.geoipConfig;
+    return this._httpClient.get(`${apiUrl}?key=${apiKey}`).pipe(
+      map(
+        (result: any) =>
+          <GeoipData>{
+            currency: {
+              code: result.currency.code,
+              symbol: result.currency.symbol,
+              symbolNative: result.currency.symbol_native,
+            },
+            location: {
+              continent: {
+                code: result.location.continent.code,
+                name: result.location.continent.name,
+              },
+              country: {
+                code: result.location.country.code,
+                name: result.location.country.name,
+                inEU: result.location.country.in_eu,
+              },
+            },
+          }
+      )
+    );
+  });
+  initialized$: Observable<boolean> = this._initializedSource.asObservable();
+
+  get value(): GeoipData | undefined {
+    return this._value;
+  }
+
+  private _value: GeoipData | undefined;
+
+  constructor(
+    private readonly _httpClient: HttpClient,
+    @Inject(EnvironmentToken)
+    private readonly _environment: Environment
+  ) {
+    this.getGeoipData();
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed.next();
+    this._destroyed.complete();
+  }
+
+  private getGeoipData(): void {
+    (this._environment.production ? this._geoipData$ : of(DEFAULT_GEOIP_DATA))
+      .pipe(
+        takeUntil(this._destroyed),
+        tap((geoipData: GeoipData) => {
+          this._value = Object.freeze(geoipData);
+          this._initializedSource.next(true);
+        })
+      )
+      .subscribe();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { enableProdMode, importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideRouter, withDebugTracing } from "@angular/router";
 import { APP_ROUTES } from "@app/routes/app-routing";
+import { GeoipAppInitializerProvider } from "@app/services/geoip-data.service";
 import { EnvironmentTokenProvider } from "@app/tokens/environment.token";
-import { GeoipTokenProvider } from "@app/tokens/geoip.token";
 import { AppComponent } from "./app/app.component";
 import { environment } from "./environments/environment";
 
@@ -15,10 +15,11 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
+
     environment.production
       ? provideRouter(APP_ROUTES)
       : provideRouter(APP_ROUTES, withDebugTracing()),
     EnvironmentTokenProvider,
-    GeoipTokenProvider,
+    GeoipAppInitializerProvider,
   ],
 }).catch((err) => console.error(err));
