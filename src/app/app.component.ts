4de6975a52eab5a746fa6e99d775173b01d711c5
kosaj <kosaj@users.noreply.github.com>
2022-11-28 00:13:23 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { GeoipDataService } from "./services/geoip-data.service";
 
 @Component({
   selector: "app-root",
@@ -8,4 +9,8 @@ import { RouterOutlet } from "@angular/router";
   template: ` <router-outlet></router-outlet> `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly _geoipDataService: GeoipDataService) {
+    console.log(_geoipDataService.value);
+  }
+}
