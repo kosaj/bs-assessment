d2ae1b7c1f5febfd793b1c22d88938768188bbc0
kosaj <kosaj@users.noreply.github.com>
2022-12-04 15:14:28 +0100

    .

--- src/app/+state/bet/bet.actions.ts
+++ src/app/+state/bet/bet.actions.ts
@@ -4,10 +4,12 @@ import { Update } from "@ngrx/entity";
 import { createAction, props } from "@ngrx/store";
 
 export const init = createAction("[Bet] Initialize", props<{ size: number }>());
+
 export const initSuccess = createAction(
   "[Bet] Initialize Success",
   props<{ bets: Array<Bet> }>()
 );
+
 export const initFailure = createAction(
   "[Bet] Initialize Failure",
   props<{ error: HttpErrorResponse }>()
