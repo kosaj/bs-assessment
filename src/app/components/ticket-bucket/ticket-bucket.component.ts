803ddc91e3f32af79e819f1e2eaf22d4c48575d9
kosaj <kosaj@users.noreply.github.com>
2022-12-05 03:40:13 +0100

    .

--- src/app/components/ticket-bucket/ticket-bucket.component.ts
+++ src/app/components/ticket-bucket/ticket-bucket.component.ts
@@ -1,11 +1,31 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-bucket',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>ticket-bucket works!</p> `,
+  imports: [],
+  template: `
+    <div class="bucket-header">
+      <span class="header-counter">
+        <!-- TODO: counter -->
+        {{ 7 }}
+      </span>
+      <span>
+        <!-- TODO: title -->
+        Tickets
+      </span>
+    </div>
+    <div class="bucket-content">
+      <!-- TODO: list of selected tickets -->
+    </div>
+    <div class="bucket-actions">
+      <input type="text" />
+      <button>
+        <span>Zaloguj się i graj o</span>
+        <span>{{ 12.34 }}</span>
+      </button>
+    </div>
+  `,
   styleUrls: ['./ticket-bucket.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
