72c547db73449b5d5f12b2b0148efe709b809391
kosaj <kosaj@users.noreply.github.com>
2022-12-05 03:17:49 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
-import { PortalModule } from '@angular/cdk/portal';
+import { ComponentPortal, PortalModule } from '@angular/cdk/portal';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { VButton } from 'src/shared/components/button/button.component';
+import { TicketBucketComponent } from './components/ticket-bucket/ticket-bucket.component';
 
 @Component({
   selector: 'app-root',
@@ -18,11 +19,12 @@ import { VButton } from 'src/shared/components/button/button.component';
       *ngIf="minWidth64em$ | async; else absoluteTemplate"
     ></ng-container>
     <aside *ngIf="minWidth64em$ | async">
-      <!--  -->
+      <ng-template [cdkPortalOutlet]="componentPortal"></ng-template>
     </aside>
 
     <ng-template #absoluteTemplate>
       <span class="absolute">
+        <ng-template [cdkPortalOutlet]="componentPortal"></ng-template>
         <button v-fab-button>-</button>
       </span>
     </ng-template>
@@ -30,6 +32,7 @@ import { VButton } from 'src/shared/components/button/button.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  readonly componentPortal = new ComponentPortal(TicketBucketComponent);
   readonly minWidth64em$: Observable<boolean> = this._breakpointObserver
     .observe(['(min-width: 64em)'])
     .pipe(map((result) => result.matches));
