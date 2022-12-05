cce24fd5bebf6e559bd3938bd44b3efcfa5d9b8c
kosaj <kosaj@users.noreply.github.com>
2022-12-05 03:13:23 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,38 @@
+import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
 import { PortalModule } from '@angular/cdk/portal';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { VButton } from 'src/shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PortalModule, VButton],
+  imports: [RouterOutlet, PortalModule, LayoutModule, NgIf, AsyncPipe, VButton],
   template: `
     <main>
       <router-outlet></router-outlet>
     </main>
-    <aside>
+    <ng-container
+      *ngIf="hideAside$ | async; else absoluteTemplate"
+    ></ng-container>
+    <aside *ngIf="hideAside$ | async">
       <!--  -->
     </aside>
 
-    <span class="absolute">
-      <button v-fab-button>-</button>
-    </span>
+    <ng-template #absoluteTemplate>
+      <span class="absolute">
+        <button v-fab-button>-</button>
+      </span>
+    </ng-template>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  readonly hideAside$: Observable<boolean> = this._breakpointObserver
+    .observe(['(min-width: 64em)'])
+    .pipe(map((result) => result.matches));
+
+  constructor(private readonly _breakpointObserver: BreakpointObserver) {}
 }
